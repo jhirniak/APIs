d0f1a0a719dc0e0e97ba716fc9c6a087b46f28ee
Jaroslaw Hirniak <j@hirniak.info>
2015-10-18 20:29:45 +0100

    Script to list keys from json (to include in potato list quicker).

---
+++ apis/get_keys.py
@@ -0,0 +1,18 @@
+#!/opt/anaconda/bin/python
+
+import json
+
+def keys(fn):
+  with open(fn) as fh:
+    data = json.load(fh)
+    for key in filter(lambda k: k not in ["errors"], data.keys()):
+      yield key
+
+if __name__ == '__main__':
+  import argparse
+
+  parser = argparse.ArgumentParser(description='Extract keys out of JSON.')
+  parser.add_argument('filename', type=str)
+  args = parser.parse_args()
+  fn = args.filename
+  print ("\n".join([ " * {}".format(key) for key in keys(fn) ]))
