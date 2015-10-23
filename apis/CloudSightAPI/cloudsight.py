0f2d6bfc74cb05e57585ef1b698f79b5423a6cc1
Jaroslaw Hirniak <j@hirniak.info>
2015-10-23 15:38:17 +0100

    Updated cloudsight api script to fetch multiple times (with command line parameters), added processing pipeline, and __init__ files.

--- apis/CloudSightAPI/cloudsight.py
+++ apis/CloudSightAPI/cloudsight.py
@@ -46,39 +46,60 @@ def postImage(picurl, geoloc=None, focus=None, lang='en', locale='en_GB'):
 
   return json.loads(response.read().decode('utf-8'))['token']
 
-def getDescription(token):
-  import json
-  import urllib.parse
-  import urllib.request
+def getDescription(token, trials=1, delay=250):
+  from time import sleep
+  from datetime import datetime
 
-  url = 'https://api.cloudsightapi.com/image_responses/{}'.format(token)
+  def getDescriptionRequest(token):
+    import json
+    import urllib.parse
+    import urllib.request
 
-  headers = {
-    'Authorization': 'CloudSight uDm0TK41qU9LS1zkrrxMVw'
-  }
+    url = 'https://api.cloudsightapi.com/image_responses/{}'.format(token)
 
-  req = urllib.request.Request(url, headers=headers)
+    headers = {
+      'Authorization': 'CloudSight uDm0TK41qU9LS1zkrrxMVw'
+    }
 
-  response = urllib.request.urlopen(req)
+    req = urllib.request.Request(url, headers=headers)
 
-  desc = json.loads(response.read().decode('utf-8'))
+    response = urllib.request.urlopen(req)
 
-  if 'name' in desc:
-    return desc['name']
+    desc = json.loads(response.read().decode('utf-8'))
+
+    if 'name' in desc:
+      return desc['name']
+
+  trial = 0
+  description = None
+  print ("Fetching description. {} attempts in {}s intervals.".format(trials, delay))
+  while not description and trial < trials:
+    sleep(delay)
+    print ("{} attempt to fetch result...".format(trial))
+    trial += 1
+    description = getDescriptionRequest(token)
+    when = str(datetime.now())
+    print ("{} @ {}".format(description, when))
+
+  return description
 
 if __name__ == "__main__":
   import argparse
 
   parser = argparse.ArgumentParser(description='Return description for image specified in URL using CloudSightAPI.')
   parser.add_argument('url', type=str, help='Image URL')
+  parser.add_argument('-t', '--trials', type=int, help='Number of attempts to fetch the result', default=1, dest='trials')
+  parser.add_argument('-d', '--delay', type=int, help='Delay in ms between result requests.', default=250, dest='delay')
   
   args = parser.parse_args()
 
   imageUrl = args.url.strip()
+  trials = args.trials
+  delay = int(args.delay / 1000.0)
 
   token = postImage(imageUrl)
 
-  desc = getDescription(token)
+  desc = getDescription(token, trials, delay)
 
   print ('Image description: {}'.format(desc))
 
---
+++ apis/processing_pipeline.py
@@ -0,0 +1,3 @@
+from CloudSightAPI.cloudsight import getDescription, postImage
+
+if __name__ == "__main__":
